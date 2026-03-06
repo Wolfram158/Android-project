cfef1a1cf2e3cac03750a1ce40a6aba1da8a7dbb
Wolfram158 <wolframalpha7134@gmail.com>
2026-03-06 13:17:10 +0300

    add activity not found catching

--- app/src/main/java/ru/vk/project/MainModel.kt
+++ app/src/main/java/ru/vk/project/MainModel.kt
@@ -1,6 +1,9 @@
 package ru.vk.project
 
+import android.content.ActivityNotFoundException
+import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 
 class MainModel : MainIntentHandler {
     override fun dispatchIntent(intent: MainIntent) {
@@ -23,7 +26,9 @@ class MainModel : MainIntentHandler {
         Intent(Intent.ACTION_DIAL).apply {
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
             setData(intent.tel.fromTelToUri())
-            intent.context.startActivity(this)
+            withActivityNotFoundCatching(intent.context) {
+                intent.context.startActivity(this)
+            }
         }
     }
 
@@ -41,8 +46,26 @@ class MainModel : MainIntentHandler {
             type = "text/plain"
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
             putExtra(Intent.EXTRA_TEXT, intent.text)
-            intent.context.startActivity(this)
+            withActivityNotFoundCatching(intent.context) {
+                intent.context.startActivity(this)
+            }
         }
     }
 
+    private inline fun withActivityNotFoundCatching(
+        context: Context,
+        crossinline tryAction: () -> Unit
+    ) {
+        try {
+            tryAction()
+        } catch (_: ActivityNotFoundException) {
+            Toast
+                .makeText(
+                    context,
+                    context.getString(R.string.activity_not_found),
+                    Toast.LENGTH_LONG
+                )
+                .show()
+        }
+    }
 }
